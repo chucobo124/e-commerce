483ffce1ed7707180f0599a5fb329e9f94d6916f
ChenLucas <chucobo5219@gmail.com>
2017-03-23 02:51:36 +0800

    create variant and product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322075036) do
+ActiveRecord::Schema.define(version: 20170322141052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name",           default: "",    null: false
+    t.string   "description",    default: "",    null: false
+    t.decimal  "price",          default: "0.0", null: false
+    t.decimal  "discount_price"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",               default: "", null: false
@@ -32,6 +41,18 @@ ActiveRecord::Schema.define(version: 20170322075036) do
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
+  end
+
+  create_table "variants", force: :cascade do |t|
+    t.string   "sku",                           null: false
+    t.integer  "count_on_hand", default: 0
+    t.string   "state"
+    t.boolean  "visible",       default: false
+    t.boolean  "is_default",    default: false
+    t.integer  "product_id"
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.index ["product_id"], name: "index_variants_on_product_id", using: :btree
   end
 
 end
