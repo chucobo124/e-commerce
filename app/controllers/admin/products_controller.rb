8599dc5df0afdefb8820bb83ff2347508fba9e16
ChenLucas <chucobo5219@gmail.com>
2017-03-23 15:09:48 +0800

    initialize admin console

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,4 @@
+class Admin::ProductsController < AdminApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions',
                                     passwords: 'users/passwords',
                                     registrations: 'users/registrations'}
+  namespace :admin do
+    root 'homes#index'
+    resources :products
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
