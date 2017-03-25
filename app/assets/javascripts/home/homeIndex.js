804a68bc2096e14d790498b5d82d96e2ec123fa3
ChenLucas <chucobo5219@gmail.com>
2017-03-25 18:33:47 +0800

    Add Add to Cart animation

---
+++ app/assets/javascripts/home/homeIndex.js
@@ -0,0 +1,79 @@
+$('body').click(function(evt) {
+    if($(evt.target).parents(".cart").length==0 &&
+       $(evt.target).parents(".cartBtn").length==0 &&
+       $(evt.target).parents(".cartItem").length==0 &&
+       !$(evt.target).hasClass('cartBtn') &&
+       !$(evt.target).hasClass('cart')) {
+
+      $('.cart').hide();
+    }
+});
+var lineItems = {};
+function addItemToLineItems(variant) {
+  var variant_id = variant.id;
+  if(lineItems[variant_id] === undefined){
+    lineItems[variant_id] = {
+      name: variant.name,
+      image: variant.image,
+      price: variant.price,
+      count: 1,
+    }
+  }
+  else{
+    lineItems[variant_id].count = lineItems[variant_id].count + 1;
+  }
+  return lineItems;
+}
+function createComponent(variant, variant_id) {
+  return `<div class="cartItem">
+    <span class="item">
+      <span class="item-left">
+          <img style="width:100px" src="` + variant.image + `" alt="" />
+          <span class="item-info">
+              <span>` + variant.name + `</span>
+              <span>`+ variant.price +`</span>
+              <span> X`+ variant.count +`</span>
+          </span>
+          <span> <span>
+      </span>
+      <span class="item-right">
+          <button onclick='deleteItem()' data-variant-id=`+ variant_id +` class="deleteBtn btn btn-xs btn-danger pull-right" href="#">x</button>
+      </span>
+    </span>
+  </div>`
+}
+function renderLineItems(lineItems){
+  $('div .cart').empty();
+  var lineItemsLayout = '';
+  for (var index in lineItems ){
+    lineItemsLayout += createComponent(lineItems[index], index);
+  }
+  if(Object.keys(lineItems).length === 0) {
+    $('div .cart').append(`<div class="cartItem"><h3> Wanna buy something ?</h3></div>`);
+  }
+  else {
+    $('div .cart').append(lineItemsLayout);
+  }
+}
+$('.addToCart').click(function(event){
+  var variant_id = $(this).attr('data-variant-id');
+  var containClass =  '.variant_' + String(variant_id);
+  var variant = {
+    id: variant_id,
+    name: $(containClass + ' .variantName').text(),
+    image: $(containClass + ' img').attr('src'),
+    price: $(containClass + ' .variantPrice').text()
+  };
+  var lineItems = addItemToLineItems(variant);
+  renderLineItems(lineItems);
+});
+
+$('.cartBtn').click(function(){
+  $('div .cart').toggle('fast');
+  renderLineItems(lineItems);
+});
+function deleteItem(){
+  variant_id = $('.deleteBtn').attr('data-variant-id');
+  delete lineItems[variant_id];
+  renderLineItems(lineItems);
+}
