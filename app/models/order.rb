a244ad7db7c004377a353d6c1516036cba3e3757
ChenLucas <chucobo5219@gmail.com>
2017-03-27 21:01:01 +0800

    Create Order Object

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,22 @@
 class Order < ApplicationRecord
+  include AASM
   belongs_to :user
   has_many :variants
+  aasm column: :state do
+    state :in_progress, initial: true
+    state :ready_to_pay
+    state :purchased
+    state :shipped
+    state :completed
+    state :error
+    event :prepare_to_pay do
+      transitions form: :in_progress, to: :ready_to_pay
+    end
+    event :shipping do
+      transitions form: :purchased, to: :shipped
+    end
+    event :complete do
+      transitions form: :shipped, to: :completed
+    end
+  end
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'prepare_to_pay' do
+    order = orders(:order_default).tap do |order|
+      order.prepare_to_pay
+    end
+    assert_equal 'ready_to_pay', order.state, 'state should return ready_to_pay'
+  end
+  test 'shipping' do
+    order = orders(:order_default).tap do |order|
+      order.shipping
+    end
+    assert_equal 'shipped', order.state, 'state should return shipped'
+  end
+  test 'complete' do
+    order = orders(:order_default).tap do |order|
+      order.complete
+    end
+    assert_equal 'completed', order.state, 'state should return completed'
+  end
 end
