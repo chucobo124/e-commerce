4f80e1a073f0f3eb0ef73100bd419394fc2b761b
ChenLucas <chucobo5219@gmail.com>
2017-04-04 12:18:55 +0800

    remove not need block.

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -6,9 +6,6 @@ class Admin::ProductsController < Admin::ApplicationController
   def create
     images = _create_image_object
     product = Product.create(_product_params)
-    images.each do |image|
-      product.images.create(asset: image)
-    end
     redirect_to admin_products_path
   end
 
@@ -25,28 +22,10 @@ class Admin::ProductsController < Admin::ApplicationController
     images = _create_image_object
     product = Product.find params[:id]
     product.update(_product_params)
-    images.each_with_index do |image, index|
-      if product.images[index].blank?
-        product.images.create(asset: image)
-      else
-        product.images[index].asset = image
-      end
-    end
     redirect_to admin_products_path
   end
 
   private
-
-  def _create_image_object
-    images = []
-    5.times do |index|
-      params.require(:product).permit(images: index.to_s).tap do |image|
-        next if image['images'].blank?
-        images << image['images'][index.to_s]
-      end
-    end
-    images
-  end
 
   def _product_params
     params.require(:product).permit(:name, :description, :price, :discount_price)
